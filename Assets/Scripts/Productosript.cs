f84f16174016445444ca2c5e4806d9a0c0e35c63
MatiasNiebieski <163850963+MatiasNiebieski@users.noreply.github.com>
2024-06-27 18:48:30 -0300

    Correccion de precios(falta terminar)

--- Assets/Scripts/Productosript.cs
+++ Assets/Scripts/Productosript.cs
@@ -2,10 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Producto : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    public int precio;
-
+    public GameObject[] prodcuctos;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/manager.cs
+++ Assets/Scripts/manager.cs
@@ -3,40 +3,108 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class manager : MonoBehaviour
+public class Manager : MonoBehaviour
+
 {
-    public Text precio1text;
-    public Text precio2text;
+    public Transform spawnPoint1;
+    public Transform spawnPoint2;
 
-    public Text platatext;
+    public int dinero = 100;
 
-    public Transform spawn1;
-    public Transform spawn2;
+    public GameObject[] productos;
 
-    public GameObject[] objetos;
+    GameObject obj1;
+    GameObject obj2;
 
-    int plata;
-    int precio1;
-    int precio2;
+    public Text presupuesto;
+    public Text pr1;
+    public Text pr2;
+
+    public GameObject panel;
+    public Text resultado;
+
+    private int sumaProductos;
 
     void Start()
     {
-        plata = Random.Range(100, 1000);
-        platatext.text = plata + "$";
-
-        int index = Random.Range(0, objetos.Length);
-        Instantiate(objetos[index], spawn1.position, Quaternion.identity);
-
-        index = Random.Range(0, objetos.Length);
-        Instantiate(objetos[index], spawn2.position, Quaternion.identity);   
+        obj1 = Instantiate(productos[Random.Range(0, productos.Length)], spawnPoint1.position, Quaternion.identity);
 
 
+        obj2 = Instantiate(productos[Random.Range(0, productos.Length)], spawnPoint2.position, Quaternion.identity);
 
+        int pre1 = obj1.GetComponent<ProductoScript>().precio; // falta corregir
+
+
+        int pre2 = obj2.GetComponent<ProductoScript>().precio; // falta corregir
+
+        sumaProductos = pre1 + pre2;
+
+        dinero = Random.Range(10, 100);
+
+        presupuesto.text = $"Presupuesto: ${dinero}";
+        pr1.text = $"${pr1}";
+        pr2.text = $"${pr2}";
     }
 
-    // Update is called once per frame
-    void Update()
+    void Verprecio(int a)
     {
-        
+        panel.SetActive(true);
+
+        if (a == 1)
+        {
+            if (dinero > sumaProductos)
+            {
+                
+                resultado.text = "Compraste";
+            }
+            else
+            {
+                
+                resultado.text = "No alcanza";
+            }
+        }
+        else if (a == 2)
+        {
+            if (dinero == sumaProductos)
+            {
+               
+                resultado.text = "Compraste";
+            }
+            else
+            {
+                
+                resultado.text = "No alcanza";
+            }
+        }
+        else if (a == 3)
+        {
+            if (dinero < sumaProductos)
+            {
+              
+                resultado.text = "Compraste";
+            }
+            else
+            {
+          
+                resultado.text = "No Alcanza";
+            }
+        }
+    }
+
+    public void ExcederValor()
+    {
+        Debug.Log("Sobra");
+        Verprecio(1);
+    }
+
+    public void Alcanza()
+    {
+        Debug.Log("Le alcanza para el producto");
+        Verprecio(2);
+    }
+
+    public void NoLlega()
+    {
+        Verprecio(3);
     }
 }
