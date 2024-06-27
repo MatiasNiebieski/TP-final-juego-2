cebe7a660f6dbd307e5d1d3fd5820a63e613b8d7
MatiasNiebieski <163850963+MatiasNiebieski@users.noreply.github.com>
2024-06-27 11:43:45 -0300

    Initial commit

---
+++ Assets/Scripts/manager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class manager : MonoBehaviour
+{
+    public Text precio1text;
+    public Text precio2text;
+
+    public Text platatext;
+
+    public Transform spawn1;
+    public Transform spawn2;
+
+    public GameObject[] objetos;
+
+    int plata;
+    int precio1;
+    int precio2;
+
+    void Start()
+    {
+        plata = Random.Range(100, 1000);
+        platatext.text = plata + "$";
+
+        int index = Random.Range(0, objetos.Length);
+        Instantiate(objetos[index], spawn1.position, Quaternion.identity);
+
+        index = Random.Range(0, objetos.Length);
+        Instantiate(objetos[index], spawn2.position, Quaternion.identity);
+
+        precio1 = Random.Range(100, 500);
+        platatext.text = plata + "$";
+
+        int index2 = Random.Range(0, objetos.Length);
+        Instantiate(objetos[index], spawn1.position, Quaternion.identity);
+
+        index = Random.Range(0, objetos.Length);
+        Instantiate(objetos[index], spawn2.position, Quaternion.identity);
+
+        precio2 = Random.Range(100, 500);
+        platatext.text = plata + "$";
+
+        int index3 = Random.Range(0, objetos.Length);
+        Instantiate(objetos[index], spawn1.position, Quaternion.identity);
+
+        index = Random.Range(0, objetos.Length);
+        Instantiate(objetos[index], spawn2.position, Quaternion.identity);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
